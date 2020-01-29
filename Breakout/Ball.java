6d130f1a8819fd0523fd595ce5f571e0dda915f1
Unknown <greg.hutchinson@fcc-fac.ca>
2020-01-29 11:38:55 -0600

    Start of course

--- Breakout/Ball.java
+++ Breakout/Ball.java
@@ -1,58 +1,36 @@
+
 import greenfoot.*;  
 public class Ball extends Actor
-{
-    private boolean stuck = true;
+{    private boolean stuck = true;
 
-    protected void addedToWorld(World world) {
-        setRotation(333);
+    public void addedToWorld(World aWorld) {
+        setRotation(45);
     }
-        
     public void act() 
     {
-        if (isStuck())
-            return;
-        makeSmoke();
         move(10);
-    }
-    public void changeXDirection() {
-        int current = getRotation();
-        setRotation(180 - current);
-    }
-    public void changeYDirection() {
-        int current = getRotation();
-        setRotation(360 - current);
-    }
-    public boolean isStuck() {
-        return stuck;
-    }
-    
-    public void struckByPaddle() {
-        if (isStuck())
-            return;
-        if (getRotation() < 180) {
-            new GreenfootSound("ping.wav").play();    
-            changeYDirection();
-        }
-    }
-    public void becomeUnstuck() {
-        stuck = false;
-    }
-    
-    private int smokeX;
-    private int smokeY;
-    private int count = 0;
-    
-    private void makeSmoke()
-    {
-        if (count == 0) {
-            smokeX = getX();
-            smokeY = getY();
-        }
         
-        if (count++ >= 2) {
-            getWorld().addObject ( new Smoke(), smokeX, smokeY);
-            count = 0;
-        }
+        
+        if (getX() >= 799)
+            setRotation(180 - getRotation());
+        if (getX() < 1)
+            setRotation(180 - getRotation());
+            
+        if (getY() >= 599)
+            setRotation(- getRotation());
+        if (getY() < 1)
+            setRotation( - getRotation());
+//        System.out.println(getWidth());
     }
+    
+    public Breakout getBreakout() {
+        return (Breakout) getWorld();
+    }
+    
+    public int getWidth() {
+        return getBreakout().getWidth();
+    }
+
 }
+    
 
